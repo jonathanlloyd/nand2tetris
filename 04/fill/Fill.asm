0b7596474d35bd12ec2571032de07f67650355f3
Jonathan Lloyd <jonathan@thisisjonathan.com>
2020-08-22 21:14:04 +0100

    Implement Fill in asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,66 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+// LOOP
+(LOOP)
+// if key is down
+//   color = -1
+// else
+//   color = 0
+@KBD
+D=M // Load keyboard into D
+@KEY_NOT_DOWN
+D;JEQ // If it's not set, jump to else
+@0 // Load 0 (intended value + 1)
+D=A
+@STORE
+0;JMP
+(KEY_NOT_DOWN)
+@1 // Load 1 (indended value + 1)
+D=A
+(STORE)
+@color
+D=D-1
+M=D
+
+// i = 0
+@0
+D=A
+@i
+M=D
+(PAINT_LOOP)
+// if i >= 8192
+//   goto END
+@i
+D=M
+@8192
+D=D-A
+@END
+D;JGE
+// m[SCREEN + i] = color
+// Store SCREEN + i -> to_paint
+@i
+D=M
+@SCREEN
+D=A+D
+@to_paint
+M=D
+// Load color
+@color
+D=M
+// Store color -> to_paint
+@to_paint
+A=M
+M=D
+
+// i++
+@i
+M=M+1
+
+@PAINT_LOOP
+0;JMP
+
+(END)
+@LOOP
+0;JMP
