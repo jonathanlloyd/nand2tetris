a1fd76e13f3b9234a07e28a9179b0cff0dfee41b
Jonathan Lloyd <jonathan@thisisjonathan.com>
2020-08-22 19:02:37 +0100

    Implement multiplication in asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,49 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@0
+D=A
+// i = 0
+@i
+M=D
+// j = 0
+@j
+M=D
+// acc = 0
+@acc
+M=D
+
+(LOOP)
+// if i >= R0
+//   goto END
+@i
+D=M
+@R0
+D=D-M
+@END
+D;JGE
+
+// i += 1
+@i
+M=M+1
+
+// ACC += R1
+@R1
+D=M
+@acc
+M=D+M
+
+// goto LOOP
+@LOOP
+0;JMP
+
+// END
+// R2 = acc
+(END)
+@acc
+D=M
+@R2
+M=D
+
+@END
+0;JMP
